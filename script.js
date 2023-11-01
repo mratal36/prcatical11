bbf36357f4bc58017aa580001ae5d7fca52e9e04
mratal36 <136173230+mratal36@users.noreply.github.com>
2023-11-01 14:43:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+
+// practical 1
+// document.write("Hello,This is our inaugral javascript code")
+
+
+// practical 2
+// var int = 12;
+// var float = 1.2;
+// var character ="a";
+// var string = "dhiraj";
+// console.log(int , float , character , string);
+
+// prcatical 3
+
+// var a = 2;
+// var b = 3;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+
+// practical 4
+
+// alert("this is a alert box to enter your name");
+// prompt("Enter your name:");
+// confirm("are you sure you want to continue");
+
+// practical 5
+// var num = prompt("enter a number:");
+// if(num%2 == 0){
+//     console.log(num+" is the even number");
+// }else{
+//     console.log(num+" it is a odd number");
+// }
+
+// practical 6
+
+// var age = prompt("enter your age:");
+// if(age>= 18){
+//         console.log("you are eligible to vote!");
+//     }else{
+//         console.log("you are not eligible to vote")
+//     }
+
+// practical 7 
+
+// var a = 12;
+// var b = 15;
+// var c = 3;
+
+// if( a>b || a >c){
+//     console.log("gretest number is a")
+// }
+
+// else if( b>a || b >c){
+//     console.log("gretest number is b")
+// }
+
+// else{
+//     console.log("gretest number is c")
+// }
+
+
+
+
